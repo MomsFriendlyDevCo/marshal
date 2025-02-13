fb980984347493e741684d0d78bdf3e23dea5a6a
Matt Carter <m@ttcarter.com>
2025-02-13 11:16:59 +1000

    refactor: Swap lodash -> lodash-es

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import _ from 'lodash';
-import _keyArrange from 'lodash-keyarrange';
-_.mixin(_keyArrange);
+import {
+	cloneDeep as _cloneDeep,
+	defaults as _defaults,
+	get as _get,
+	keys as _keys,
+	isObject as _isObject,
+	mapKeys as _mapKeys,
+	set as _set,
+} from 'lodash-es';
+import {keyArrangeDeep} from 'lodash-keyarrange';
 
 import modDate from '#modules/date';
 import modFunction from '#modules/function';
@@ -39,7 +46,7 @@ let marshal = {
 	*/
 	loadModules(modules) {
 		return modules.map(module => {
-			if (_.isObject(module)) { // Assume its a compatible module
+			if (_isObject(module)) { // Assume its a compatible module
 				if (!module.id) throw new Error('Provided custom module does not expose an `id` property');
 				return marshal.loadedModules[module.id] = module;
 			} else {
@@ -49,22 +56,22 @@ let marshal = {
 	},
 
 	serialize(data, options) {
-		let settings = _.defaults(options, marshal.settings);
+		let settings = _defaults(options, marshal.settings);
 		let modules = marshal.loadModules(settings.modules);
 
 		let tree =
-			settings.symetric ? _.keyArrangeDeep(data)
-			: settings.clone ? _.cloneDeep(data)
+			settings.symetric ? keyArrangeDeep(data)
+			: settings.clone ? _cloneDeep(data)
 			: data;
 
 		let seen = new Map();
 
 		let traverse = (node, path) => {
-			let isObject = _.isObject(node);
+			let isObject = _isObject(node);
 			if (isObject && settings.circular) {
 				let foundExisting = seen.get(node);
 				if (foundExisting) {
-					return _.set(tree, path, {_: '~circular', p: foundExisting.join('.')});
+					return _set(tree, path, {_: '~circular', p: foundExisting.join('.')});
 				} else {
 					seen.set(node, path);
 				}
@@ -76,17 +83,17 @@ let marshal = {
 			if (encoder) {
 				let result = encoder.serialize(node);
 				if (encoder.recursive && isDeep) {
-					let keys = _.keys(node);
+					let keys = _keys(node);
 					if (settings.symetric) keys.sort();
-					_.set(tree, path, result);
+					_set(tree, path, result);
 					keys.forEach(k => traverse(node[k], path.concat(k)));
 				} else if (path.length) {
-					_.set(tree, path, result);
+					_set(tree, path, result);
 				} else {
 					tree = result;
 				}
 			} else if (isDeep) {
-				let keys = _.keys(node);
+				let keys = _keys(node);
 				if (settings.symetric) keys.sort();
 				keys.forEach(k => traverse(node[k], path.concat(k)));
 			}
@@ -98,33 +105,33 @@ let marshal = {
 	},
 
 	deserialize(data, options) {
-		let settings = _.defaults(options, marshal.settings);
+		let settings = _defaults(options, marshal.settings);
 		let modules = marshal.loadModules(settings.modules);
-		let modulesByID = _.mapKeys(modules, 'id');
+		let modulesByID = _mapKeys(modules, 'id');
 
 		let tree = settings.destringify ? JSON.parse(data)
-			: settings.clone ? _.cloneDeep(data)
+			: settings.clone ? _cloneDeep(data)
 			: data;
 
 		let traverse = (node, path) => {
-			if (_.isObject(node) && node._ && node._.startsWith('~') && modulesByID[node._]) {
+			if (_isObject(node) && node._ && node._.startsWith('~') && modulesByID[node._]) {
 				let result = modulesByID[node._].deserialize(node);
 				if (path.length) {
-					if (modulesByID[node._].recursive && _.isObject(result)) { // If we got back an object should we recurse into it?
-						_.set(tree, path, result);
-						_.keys(result).forEach(k =>
+					if (modulesByID[node._].recursive && _isObject(result)) { // If we got back an object should we recurse into it?
+						_set(tree, path, result);
+						_keys(result).forEach(k =>
 							traverse(node[k], [...path, k])
 						);
 					} else { // Simple object return (or we're not recursing)
-						_.set(tree, path, result);
+						_set(tree, path, result);
 					}
 				} else {
 					tree = result;
 				}
-			} else if (settings.circular && _.isObject(node) && node._ && node._ == '~circular') {
-				_.set(tree, path, _.get(tree, node.p));
-			} else if ((!settings.depth || path.length < settings.depth) && _.isObject(node)) {
-				_.keys(node).forEach(k => traverse(node[k], path.concat(k)));
+			} else if (settings.circular && _isObject(node) && node._ && node._ == '~circular') {
+				_set(tree, path, _get(tree, node.p));
+			} else if ((!settings.depth || path.length < settings.depth) && _isObject(node)) {
+				_keys(node).forEach(k => traverse(node[k], path.concat(k)));
 			}
 		};
 
